90dc7bac7040971fa8aa5d4ba6ca7477caebba94
ArielK <arielkarmazin@gmail.com>
2018-04-23 16:22:23 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+var input;
+var count = 0;
+var sum = 0;
+
+function hitMe() {
+    return Math.floor((Math.random()*13)+1); // generate random number between 1 and 13
+}
+
+function checkScore() {
+    if (sum > 21) {
+        console.log('your score is:', sum, 'you lose!');
+        process.exit();
+    } else {
+        console.log('your score is:', sum);
+        ask();
+    }
+}
+function ask() {
+    if (count < 2) {
+        rl.question('type "hit" to draw a card or "stay" to stay: ', (answer) => {
+            count++;
+            if (answer === 'hit') {
+                sum += hitMe();
+                checkScore();
+            } else if (answer === 'stay') {
+                console.log('Your score is:', sum);
+                process.exit();
+            } else {
+                console.log('Please type "hit" or "stay"');
+                ask();
+            }
+        });
+    } else {
+        if (sum > 21) {
+            console.log('your score is:', sum, 'you lose!');
+        } else {
+            console.log('your score is:', sum);
+        }
+        process.exit();
+    }
+}
+// start here
+ask();
+
